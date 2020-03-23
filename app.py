b28126c9b1d455a623d959ebcf79b80af6ea8826
Nolan <nolanzipf@gmail.com>
2020-03-22 19:47:26 -0700

    new api

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_restful import Api, Resource, reqparse
+
+import start
+
+app = Flask(__name__)
+
+
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    parser = reqparse.RequestParser()
+    parser.add_argument("id")
+    parser.add_argument("link")
+    args = parser.parse_args()
+    id = args['id']
+    link = args['link']
+    payload = start.getLinks(id, link), 200
+
+    return payload
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
