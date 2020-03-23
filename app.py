43d28a13201db6d40f0925f9e7e832e9793a9e6f
Nolan <nolanzipf@gmail.com>
2020-03-22 20:21:07 -0700

    changed api

--- app.py
+++ app.py
@@ -6,37 +6,18 @@ from flask_restful import Api, Resource, reqparse
 import start
 
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/post/', methods=['POST'])
+def post_something():
+    parser = reqparse.RequestParser()
+    parser.add_argument("id")
+    parser.add_argument("link")
+    args = parser.parse_args()
+    id = args['id']
+    link = args['link']
+    payload = start.getLinks(id, link), 200
 
-class Video(Resource):
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("id")
-        parser.add_argument("link")
-        args = parser.parse_args()
-        id = args['id']
-        link = args['link']
-        payload = start.getLinks(id, link), 200
-
-        return payload
-
-   
-api.add_resource(Video, "/post")
-
-
-# @app.route('/post/', methods=['POST'])
-# def post_something():
-#     parser = reqparse.RequestParser()
-#     parser.add_argument("id")
-#     parser.add_argument("link")
-#     args = parser.parse_args()
-#     id = args['id']
-#     link = args['link']
-#     payload = start.getLinks(id, link), 200
-
-#     return payload
+    return payload
 
 # A welcome message to test our server
 @app.route('/')
--- browser.py
+++ browser.py
@@ -14,15 +14,15 @@ def createBrowser(headless, local):
 
     option = webdriver.ChromeOptions()
     option.binary_location = bin
+    option.add_argument('--disable-gpu')
+    option.add_argument('--no-sandbox')
+    option.add_argument('--window-size=800,600')
     option.add_argument('--log-level=3')
     option.add_argument("--incognito")
     option.add_argument("--mute-audio")
-    option.add_argument('--disable-gpu')
-    option.add_argument('--no-sandbox')
     option.add_argument("--disable-popup-blocking")
     option.add_argument("--disable-extensions")
     option.add_argument("--disable-application-cache")
-    
     
     if setup.local():
         option.add_argument("--headless")
