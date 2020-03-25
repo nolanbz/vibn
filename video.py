cba1a29be44e3717edb9c42f72a170d962c8b375
Nolan <nolanzipf@gmail.com>
2020-03-24 21:02:13 -0700

    added try catch to show more button

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 def get(driver, video_url):
@@ -10,16 +10,17 @@ def get(driver, video_url):
     button_class_name = "more-button"
     video_path = "//yt-formatted-string[@class='style-scope ytd-video-primary-info-renderer']"
 
+    
+
     try:
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, video_path)))
-
         show_more_button = driver.find_elements_by_class_name(button_class_name)
-        if len(show_more_button) > 0:
-            for button in show_more_button:
-                button.click()
-        else:
-            return False
 
+        try:
+            show_more_button.click()
+        except NoSuchElementException:
+            print(video_url)
+            return False
         return True
 
     except TimeoutException:
