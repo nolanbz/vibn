62aa68b5159329f68aaf24aae663a10fd269513b
Nolan <nolanzipf@gmail.com>
2020-03-24 22:21:01 -0700

    error handles show more button

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
 
 def get(driver, video_url):
@@ -14,14 +14,13 @@ def get(driver, video_url):
 
     try:
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, video_path)))
-        show_more_button = driver.find_element_by_class_name(button_class_name)
-
+        
         try:
+            show_more_button = driver.find_element_by_class_name(button_class_name)
             show_more_button.click()
-        except NoSuchElementException:
-            print(video_url)
+            return True
+        except:
             return False
-        return True
 
     except TimeoutException:
         print("Timed out waiting for video to load")
